64f3595dfe307d4e027f3ad4b44143451b147c9a
Clauveira <claue.silveira@gmail.com>
2021-09-25 16:27:10 -0300

    Support for Character assets selection Player movement and animation

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1.5f;
     public Rigidbody2D rigidbody;
+    public Animator animator;
 
     Vector2 movement;
 
@@ -14,13 +15,35 @@ public class PlayerMovement : MonoBehaviour
     }
     void Update()
     {
+        Move();
+        Animate();
+    }
+
+    void Move()
+    {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+    }
 
+    void Animate()
+    {
+        if (movement.sqrMagnitude > 0)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            if (movement.x == 0)
+            {
+                animator.SetFloat("Vertical", movement.y);
+            }
+            else
+            {
+                animator.SetFloat("Vertical", 0);
+            }
+        }
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
-        rigidbody.MovePosition(rigidbody.position + movement * speed * Time.fixedDeltaTime);
+        rigidbody.MovePosition(rigidbody.position + movement.normalized * speed * Time.fixedDeltaTime);
     }
 }
